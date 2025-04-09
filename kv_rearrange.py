535f49e2922b573dc5f930c6127e611b8c5625f6
Jingxin Pan <jingxin.pan123@gmail.com>
2025-04-09 19:38:22 +0000

    update bench

--- kv_rearrange.py
+++ kv_rearrange.py
@@ -2,7 +2,7 @@ import torch
 import triton
 import triton.language as tl
 import rearrange_cuda 
-
+import time
 
 
 @triton.jit
@@ -57,6 +57,7 @@ def rearrange_kernel_write(
 
     block_start = pid * BLOCK_SIZE
     offsets = block_start + tl.arange(0, BLOCK_SIZE)
+    mask = offsets < (N * B * H * C)
 
     curr_n = offsets // block_size
     curr_b = offsets // token_size % B
@@ -71,7 +72,11 @@ def rearrange_kernel_write(
 
     dst_pos = tensor_subset_size * tp_group + tp_group_offset
 
-    tl.store(t2_ptr + dst_pos, tl.load(t1_ptr + src_pos))
+    #FIXME: This is a hack to avoid the issue with the compiler
+    src_pos = curr_n * block_size + curr_b * token_size + curr_h * C + curr_c
+
+    tl.store(t2_ptr + dst_pos, tl.load(t1_ptr + src_pos, mask), mask=mask)
+
 
 
 
@@ -116,32 +121,74 @@ def rearrange_tensors_to_dram(t1: torch.Tensor, t2: torch.Tensor, d: int):
     assert t2.shape == (N, B, H, C), "Destination tensor must have the same shape as source"
     assert H % d == 0, "H must be divisible by d"
     assert t1.device.type == "cuda", "t1 must be on GPU"
-    assert t2.device.type == "cpu", "t2 must be on CPU"
+    # assert t2.device.type == "cpu", "t2 must be on CPU"
 
     heads_per_group = H // d
 
     t2.copy_(t1.reshape(N, B, d, heads_per_group, C)
                .permute(2, 0, 1, 3, 4)
-               .contiguous()
                .reshape(N, B, H, C))
 
 
-N, B, H, C, d = 1, 2, 4, 3, 2
-t1 = torch.arange(N * B * H * C, device="cuda", dtype=torch.float16).reshape(N, B, H, C)
-print(t1.dtype)
-t2 = torch.empty_like(t1)
 
-rearrange_tensors(t1, t2, d, "write")
-print(t2)
+def test():
+    N, B, H, C, d = 1, 2, 4, 3, 2
+    t1 = torch.arange(N * B * H * C, device="cuda", dtype=torch.float16).reshape(N, B, H, C)
+    print(t1.dtype)
+    t2 = torch.empty_like(t1)
 
-
-t3 = torch.empty_like(t1, device="cuda")
-rearrange_cuda.rearrange_tensors_cuda(t1, t3, d)
-print(t3)
+    rearrange_tensors(t1, t2, d, "write")
+    print(t2)
 
 
-t4 = torch.empty_like(t1, device="cpu")
-# print(t1.device)
-# print(t2.device)
-rearrange_tensors_to_dram(t1, t4, d)
-print(t4)
+    t3 = torch.empty_like(t1, device="cuda")
+    rearrange_cuda.rearrange_tensors_cuda(t1, t3, d)
+    print(t3)
+
+
+    t4 = torch.empty_like(t1, device="cpu")
+    # print(t1.device)
+    # print(t2.device)
+    rearrange_tensors_to_dram(t1, t4, d)
+    print(t4)
+
+
+def benchmark():
+    N, B, H, C, d = 512, 16, 32, 4096, 2
+    t1 = torch.arange(N * B * H * C, device="cuda", dtype=torch.float16).reshape(N, B, H, C)
+
+    torch.cuda.synchronize()
+    t2 = torch.empty_like(t1)
+    start = time.perf_counter()
+    rearrange_cuda.rearrange_tensors_cuda(t1, t2, d)
+    torch.cuda.synchronize()
+    end = time.perf_counter()
+    print(f"CUDA rearrange took {end - start:.4f} seconds")
+    del t2
+
+
+
+    t3 = torch.empty_like(t1)
+    torch.cuda.synchronize()
+    start = time.perf_counter()
+    rearrange_tensors(t1, t3, d, "write")
+    torch.cuda.synchronize()
+    end = time.perf_counter()
+    print(f"Triton rearrange took {end - start:.4f} seconds")
+    del t3
+
+
+    t4 = torch.empty_like(t1)
+    torch.cuda.synchronize()
+    start = time.perf_counter()
+    rearrange_tensors_to_dram(t1, t4, d)
+    end = time.perf_counter()
+    print(f"pytorch rearrange took {end - start:.4f} seconds")
+
+
+
+#warmup
+for i in range(10):
+    test()
+
+benchmark()
